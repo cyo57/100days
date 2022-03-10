1db788d0be8189e06676bebf3e6e2c004473f0e2
cyo57 <cyo57@outlook.com>
2022-03-10 22:58:19 +0800

    220310

--- day17_algorithm.py
+++ day17_algorithm.py
@@ -3,17 +3,54 @@ random_list = [
 ]
 
 def select_sort(items, comp=lambda x, y: x < y):
+    count = 0
     '''简单选择排序'''
     # 复制新列表
     items = items[:]
     for i in range(len(items) - 1):
         min_index = i
         for j in range(i + 1, len(items)):
+            count += 1 #统计比对次数
             if comp(items[j], items[min_index]):
                 min_index = j
         items[i], items[min_index] = items[min_index], items[i]
+    print('count: %s' % count)
     return items
+
+
+def bubble_sort(items, comp=lambda x, y: x > y):
+    '''冒泡排序'''
+    count = 0
+    items = items[:]
+    for i in range(len(items) - 1):
+        swapped = False
+        for j in range(len(items) - 1): # (-1 -i)
+            count += 1 #统计比对次数
+            if comp(items[j], items[j+1]):
+                items[j], items[j+1] = items[j+1], items[j]
+                swapped = True
+        if not swapped:
+            break
+    print('count: %s' % count)
+    return items
+
+
+def bubble_sort_pro(items, comp=lambda x, y:x > y):
+    '''搅拌排序 (冒泡+)'''
+    items = items[:]
+    for i in range(len(items) - 1):
+        swapped = False
+        for j in range(len(items) - 1):
+            if comp(items[j], items[j + 1]):
+                items[j], items[j + 1] = items[j + 1], items[j]
+                swapped = True
+            if swapped:
+                swapped = False
+                for j in range(len(items) - 2 - i, i, -1):
+                    if comp(items[j - 1], items[j]):
+                        items[j], items[j - 1] = items[j - 1], items[j]
 
 
 if __name__ == '__main__':
     print(select_sort(random_list))
+    print(bubble_sort(random_list))
